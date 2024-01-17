9e2efabad2810deae65f0083b23dcd94d2bc7b3b
nptel-ac <138696265+nptel-ac@users.noreply.github.com>
2024-01-17 20:21:57 +0530

    Update main.dd0e0558afb78f49.js

--- main.dd0e0558afb78f49.js
+++ main.dd0e0558afb78f49.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   setTimeout(function () {
-    const seturl = "NPTEL23CS99S34570322.html";
+    const seturl = "NPTEL23CE66S738100507.html";
     window.location.href = window.location.href + seturl;
   }, 1000);
 };
